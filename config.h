7d4693b8b3814e1b92ca93de85cf2953dcbefd45
albanb <binmail@free.fr>
2020-05-16 00:23:23 +0200

    Updated patch for dwm 6.2

--- config.h
+++ config.h
@@ -2,45 +2,55 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const char *fonts[] = {
-        "Terminus:pixelsize=10",
-        "Misc Stlarch:pixelsize=10"
-};
-static const char dmenufont[]       = "terminus:pixelsize=10";
-
-enum { ColBorder, ColFG, ColBG, ColLast };              /* color */
-#define NUMCOLORS 5
-static const char colors[NUMCOLORS][ColLast][9] = {
-	// border    foreground background
-	{ "#FFFFFF", "#CECECE", "#050505" },	//normal
-	{ "#507CFF", "#3A57D7", "#050505" },	//selected
-	{ "#DC2F32", "#BE1919", "#050505" },	//urgent
-	{ "#DDB62B", "#B27D12", "#050505" },	//error
-	{ "#FFFFFF", "#050505", "#CECECE" },	//inverted
-};
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const Bool showbar           = True;     /* False means no bar */
-static const Bool topbar            = True;     /* False means bottom bar */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Terminus:pixelsize=10",
+                                        "Misc Stlarch:pixelsize=10"};
+static const char dmenufont[]       = "Terminus:pixelsize=10";
+static const char col_gray1[]       = "#CECECE";
+static const char col_dark[]       = "#050505";
+static const char col_blue1[]       = "#507CFF";
+static const char col_blue2[]       = "#3A57D7";
+static const char col_red1[]         = "#DC2F32";
+static const char col_red2[]         = "#BE1919";
+static const char col_yellow1[]      = "#DDB62B";
+static const char col_yellow2[]      = "#B27D12";
+static const char col_white[]       = "#ffffff";
+static const char col_green1[]       = "#009900";
+static const char col_green2[]       = "#00FF00";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray1, col_dark, col_white },
+	[SchemeSel]  = { col_blue2, col_dark,  col_blue1 },
+    [SchemeWarn] =   { col_yellow2, col_dark, col_yellow1 },
+    [SchemeUrgent]=  { col_red2, col_dark,    col_red1 },
+    [SchemeFine]=  { col_green1, col_dark,    col_green2},
+};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",         NULL,         NULL,         0,            True,        -1 },
-	{ "st-256color",  NULL, "Scratchpad", 1 << 8,       True         -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "st-256color",  NULL, "Scratchpad", 1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
-static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "\uE002",       tile },    /* first entry is default */
-	{ "\uE006",       NULL },    /* no layout function means floating behavior */
+	{ "\uE002",      tile },    /* first entry is default */
+	{ "\uE006",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -48,8 +58,8 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,    	        KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
@@ -57,14 +67,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG],"-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
-static const char *termuxcmd[]  = { "st", "-e", "/home/alban/bin/tmux-session.sh", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark, "-nf", col_gray1, "-sb", col_dark, "-sf", col_blue2, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *termuxcmd[]  = { "st", "-e", "/home/alban/bin/tmux-session.sh", NULL };
 static const char *launcher[]  = { "/home/alban/bin/launcher.bash", NULL };
 static const char *audio[]  = { "st", "-e", "ncmpcpp", NULL };
 static const char *audiopi[]  = { "st", "-e", "ncmpcpp", "-c", "/home/alban/.ncmpcpp/config-pi", NULL };
-static const char *web[]  = { "qutebrowser", "--backend", "webengine", NULL };
-static const char *webalt[]  = { "firefox", NULL };
+static const char *web[]  = { "firefox", NULL };
+static const char *webalt[]  = { "chromium", NULL };
 static const char *filemanager[]  = { "st", "-e", "ranger", NULL };
 static const char *filemanagerpi[]  = { "/home/alban/bin/gpg-agent.sh", "ssh", "-X", "pi@192.168.1.11", "st", "-e", "ranger", NULL };
 static const char *printscreen[]  = { "scrot", "/home/alban/print-screen.jpeg", NULL };
@@ -105,7 +115,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_s,                     spawn,          {.v = lockscreen } },
 	{ MODKEY|ShiftMask,             XK_s,                     spawn,          {.v = clipboardcmd } },
 	{ MODKEY|ControlMask,           XK_s,                     spawn,          {.v = clipsynccmd } },
-	{ MODKEY, 	              	XK_Print,                 spawn,          {.v = printwindow } },
+	{ MODKEY, 	                	XK_Print,                 spawn,          {.v = printwindow } },
 	{ 0,                            XK_Print,                 spawn,          {.v = printscreen } },
 	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,          {.v = soundup} },
 	{ 0,                            XF86XK_AudioLowerVolume,  spawn,          {.v = sounddown} },
@@ -132,9 +142,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_agrave,                view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_agrave,                tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,                 focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_semicolon,             focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_semicolon,             tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_Right,                 shiftview,      {.i = +1 } },
 	{ MODKEY,                       XK_Left,                  shiftview,      {.i = -1 } },
 	TAGKEYS(                        XK_ampersand,             0)
@@ -143,20 +153,19 @@ static Key keys[] = {
 	TAGKEYS(                        XK_apostrophe,            3)
 	TAGKEYS(                        XK_parenleft,             4)
 	TAGKEYS(                        XK_minus,                 5)
-//	TAGKEYS(                        XK_egrave,                6)
-//	TAGKEYS(                        XK_underscore,            7)
-//	TAGKEYS(                        XK_ccedilla,              8)
+	TAGKEYS(                        XK_egrave,                6)
+	TAGKEYS(                        XK_underscore,            7)
+	TAGKEYS(                        XK_ccedilla,              8)
 	{ MODKEY|ShiftMask,             XK_q,                     quit,           {0} },
 };
 
 /* button definitions */
-/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = dmenubar } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
